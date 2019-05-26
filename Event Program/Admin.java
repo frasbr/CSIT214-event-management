7eee41ffeb0dda6b92b31d8f66494b9855458ee9
Fraser <frasbr@gmail.com>
2019-05-26 17:16:24 +1000

    new master

--- Event Program/Admin.java
+++
@@ -1,8 +0,0 @@
-import java.io.Serializable;
-
-public class Admin extends Account implements Serializable {
-	/// Constructor
-	public Admin(String _username, String _password) {
-		super(_username, _password);
-	}
-}
